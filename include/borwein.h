ee292c5a4fcf8d1a5f5bd1b9ed30c5596a07f561
Hic-69 <franck.avoce@epitech.eu>
2026-08-23 14:00:31 +0100

    refacto : bowrwein.h

--- include/borwein.h
+++ include/borwein.h
@@ -12,8 +12,10 @@
     #include <stdbool.h>
     #include <string.h>
     #include <math.h>
+    #include <stdlib.h>
 
 bool display_help(int fd);
 bool write_depend_on_output(int fd);
 double f_n(int n, double x);
+bool parse_n(const char *str, int *out);
 #endif /*BORWEIN_H_*/

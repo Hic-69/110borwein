f40a9aadbf051faecd3327a0fa2963dc1c7da042
Hic-69 <franck.avoce@epitech.eu>
2026-08-24 09:52:07 +0100

    refacto: include/borwein.h-> add of prototype for midpoint

--- include/borwein.h
+++ include/borwein.h
@@ -14,8 +14,14 @@
     #include <math.h>
     #include <stdlib.h>
 
+    #define BORWEIN_LOWER_BOUND 0.0
+    #define BORWEIN_UPPER_BOUND 5000.0
+    #define BORWEIN_SUBINTERVALS 10000
+
 bool display_help(int fd);
 bool write_depend_on_output(int fd);
 double f_n(int n, double x);
 bool parse_n(const char *str, int *out);
+double integrate_midpoint(int n, double a, double b, int steps);
+void display_result(const char *name, int n, double value);
 #endif /*BORWEIN_H_*/
